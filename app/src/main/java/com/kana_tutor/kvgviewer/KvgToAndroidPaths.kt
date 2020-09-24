d2b7c52592fa159f61c951ee7e1b5cb2cd60f652
sjs <sjs@sonic.net>
2020-09-24 08:06:12 -0700

    Checkpoint.  Pre SvgPath.

--- app/src/main/java/com/kana_tutor/kvgviewer/KvgToAndroidPaths.kt
+++ app/src/main/java/com/kana_tutor/kvgviewer/KvgToAndroidPaths.kt
@@ -3,70 +3,168 @@ package com.kana_tutor.kvgviewer
 import android.content.Context
 import android.graphics.Path
 import android.util.Log
+import java.lang.Exception
+import java.util.*
 
-// Convert the input Kvg file to a set of Android graphic paths.
-// Each output path corresponds to a path in the Kvg file.
-class KvgToAndroidPaths(context: Context, renderChar: String) {
-    val strokePaths:Array<Path>
+
+private class BezierCurve(
+    val x0:Float, val y0:Float,
+    val x1:Float, val y1:Float,
+    val x2:Float, val y2:Float,
+    val isRelative: Boolean)
+{
+    val xR = (2*x2) - x1
+    val yR = (2*y2) - y1
+    val xM = x2; val yM = y2
+    var isEmpty = false
+    fun apply(path:Path) : BezierCurve {
+        with (path) {
+            if (isRelative) {
+                rCubicTo(x0,y0,x1,y1,x2,y2)
+            }
+            else {
+                cubicTo(x0,y0,x1,y1,x2,y2)
+            }
+            this@BezierCurve.isEmpty = false
+        }
+        return this
+    }
+    fun applyShortHand(xx0:Float, yy0:Float, xx1:Float, yy1:Float,
+                       path:Path, isRelative:Boolean) : BezierCurve
+    {
+        if (isEmpty) {
+            throw java.lang.RuntimeException(
+                "Attempt to \"applyShorthand\" with empty object")
+        }
+        if (isRelative != this.isRelative) {
+            throw java.lang.RuntimeException(
+                if (isRelative) "Attempt to absolute\"applyShorthand\" with relative object"
+                else "Attempt to absolute\"applyShorthand\" with relative object")
+        }
+        with (path) {
+            if (isRelative) {
+                rMoveTo(xM, yM)
+                rCubicTo(xR,yR,xx0,yy0,xx1,yy1)
+            }
+            else {
+                moveTo(xM,yM)
+                cubicTo(x0,y0,x1,y1,x2,y2)
+            }
+            this@BezierCurve.isEmpty = true
+        }
+        return this
+    }
+
+    constructor() : this(0f,0f,0f,0f,
+        0f,0f, false) {isEmpty = true}
+
+}
+
+class KvgToAndroidPaths(context: Context, val renderChar: String) {
+    var strokePaths:Array<Path>
     val charPath = Path()
+    var width = 0f
+        private set
+    var height = 0f
+        private set
+
+    // convert a string with kanjiVg paths to Android paths.
+    private fun List<String>.toPath( ): Path {
+        val path = Path()
+        for (stroke in this) {
+            val mr = "([a-zA-Z])([^a-zA-Z]+)".toRegex().find(stroke)
+            val (op, floatString) = mr!!.destructured
+            val coord = "(-?\\d+(?:\\.\\d+)*)".toRegex()
+                .findAll(floatString)
+                .map { it.groupValues[1].toFloat() }
+                .toList()
+                .toFloatArray()
+            var previous = BezierCurve()
+            with (path) {
+                when (op) {
+                    "m" -> rMoveTo(coord[0], coord[1])
+                    "M" -> moveTo(coord[0], coord[1])
+                    "c" -> {
+                        previous = BezierCurve(
+                            coord[0], coord[1],
+                            coord[2], coord[3],
+                            coord[4], coord[5],
+                            true)
+                        .apply(this)
+                    }
+                    "C" -> {
+                        previous = BezierCurve(
+                            coord[0], coord[1],
+                            coord[2], coord[3],
+                            coord[4], coord[5],
+                            false)
+                        .apply(this)
+                    }
+                    "s" -> {
+                        previous.applyShortHand(
+                            coord[0], coord[1],
+                            coord[2], coord[3],
+                            this, true
+                        )
+                    }
+                    "S" -> {
+                        previous.applyShortHand(
+                            coord[0], coord[1],
+                            coord[2], coord[3],
+                            this, false
+                        )
+                    }
+                    "l" -> rLineTo(coord[0], coord[1])
+                    "L" -> lineTo(coord[0], coord[1])
+                    else -> throw MissingResourceException (
+                        "${renderChar}: operator:$op, Stroke:\"$stroke\"", javaClass.simpleName,
+                        "Stroke:$stroke")
+                }
+            }
+        }
+        return path
+    }
     init {
         val paths = mutableListOf<Path>()
         val charsIn = renderChar.toCharArray()
-        val fnameIn = String.format("paths/kanjivg.kana.%04x.pat", charsIn[0].toInt())
+        val fnameIn = String.format("paths/%05x.pat", charsIn[0].toInt())
         val reader = context.assets.open(fnameIn).bufferedReader()
         var lineNumber = 0
         var line = reader.readLine()
         while (line != null) {
             lineNumber++
             if (!line.matches("^\\s*#.*".toRegex())) {
-                val lineSplit = line.split("\\s+".toRegex())
-                if (lineSplit[0] != renderChar) {
-                    throw java.lang.RuntimeException(
-                        "parsePathsFile: Bad line $lineNumber: expected $renderChar but found ${lineSplit[0]}"
-                    )
-                }
-                if (lineSplit.size == 2) {
-                    // First line of path.  Consists of path and number of paths in file.
-                    Log.d("kvgToAndroidPath", "char  = \"${lineSplit[0]}\"")
-                }
-                else {
-                    paths.add(convertPath(lineSplit.slice(1..lineSplit.lastIndex)))
+                val match = "^(\\S+)\\s+(.*)".toRegex().find(line)
+                if (match != null) {
+                    val (ch, strokesStr) = match.destructured
+                    if (width == 0f) {
+                        val m = "\\d+".toRegex().findAll(strokesStr).toList()
+                        if (m.size == 2) {
+                            width = m[0].value.toFloat()
+                            height = m[1].value.toFloat()
+                        }
+                        else throw java.lang.RuntimeException(
+                            "KvgToAndroidPaths:Failed to find width/height on first line.")
+                    }
+                    else {
+                        val strokes = "([a-zA-Z][^a-zA-Z]+)".toRegex()
+                            .findAll(strokesStr)
+                            .map { it.groupValues[0] }
+                            .toList()
+                        try {
+                            paths.add(strokes.toPath())
+                        }
+                        catch (e : Exception) {
+                            throw RuntimeException(
+                                "File $fnameIn, line $lineNumber, path exception:${e.message}")
+                        }
+                    }
                 }
             }
             line = reader.readLine()
         }
         Log.d("kvgToAndroidPath", "found ${paths.size}")
         strokePaths = paths.toTypedArray()
-    }
-    // convert a string with kanjiVg paths to Android paths.
-    private fun convertPath(strokes: List<String>): Path {
-        val path = Path()
-        for (stroke in strokes) {
-            val (op, floatStr) = stroke.split(":".toRegex())
-            val coord = floatStr
-                .split(",".toRegex())
-                .map { it.toFloat() }
-                .toFloatArray()
-            for (p in arrayOf(path, charPath)) {
-                when (op) {
-                    "m" -> p.rMoveTo(coord[0], coord[1])
-                    "M" -> p.moveTo(coord[0], coord[1])
-                    "c" -> p.rCubicTo(
-                        coord[0], coord[1],
-                        coord[2], coord[3], coord[4], coord[5]
-                    )
-                    "C" -> p.cubicTo(
-                        coord[0], coord[1], coord[2], coord[3],
-                        coord[4], coord[5]
-                    )
-                    "l" -> p.rLineTo(coord[0], coord[1])
-                    "L" -> p.lineTo(coord[0], coord[1])
-                    else -> throw RuntimeException(
-                        "Translate paths: Unrecognized operator: \"$op\""
-                    )
-                }
-            }
-        }
-        return path
+        strokePaths.forEach { charPath.addPath(it) }
     }
 }
