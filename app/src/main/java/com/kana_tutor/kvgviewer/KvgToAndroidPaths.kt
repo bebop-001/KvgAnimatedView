97c33ea0929a9cc6c9c661fa0ccc0f633a6a944d
sjs <sjs@sonic.net>
2020-09-24 09:29:52 -0700

    SvgPath is working for simple paths.

--- app/src/main/java/com/kana_tutor/kvgviewer/KvgToAndroidPaths.kt
+++ app/src/main/java/com/kana_tutor/kvgviewer/KvgToAndroidPaths.kt
@@ -3,64 +3,93 @@ package com.kana_tutor.kvgviewer
 import android.content.Context
 import android.graphics.Path
 import android.util.Log
-import java.lang.Exception
-import java.util.*
 
+class BadArgsException(message:String)
+    : Exception (message) {
+    constructor(fname:String, op:String, found:Int, expected:Int) :
+            this(String.format("file:%s op:%s expected %d args found %d",
+                fname, op, found, expected))
+    constructor(op:String, found:Int, expected:Int) :
+            this(String.format("op:%s expected %d args found %d",
+                op, found, expected))
+}
+private operator fun FloatArray.component6() = this[5]
 
-private class BezierCurve(
-    val x0:Float, val y0:Float,
-    val x1:Float, val y1:Float,
-    val x2:Float, val y2:Float,
-    val isRelative: Boolean)
-{
-    val xR = (2*x2) - x1
-    val yR = (2*y2) - y1
-    val xM = x2; val yM = y2
-    var isEmpty = false
-    fun apply(path:Path) : BezierCurve {
-        with (path) {
-            if (isRelative) {
-                rCubicTo(x0,y0,x1,y1,x2,y2)
-            }
-            else {
-                cubicTo(x0,y0,x1,y1,x2,y2)
-            }
-            this@BezierCurve.isEmpty = false
-        }
+private class SvgPath : Path() {
+    private var previousOp = ""
+    var xR  = 0f; var yR = 0f; var xM = 0f; var yM = 0f
+
+    private fun FloatArray.check(op : String,  expected:Int) : FloatArray {
+        if (this.size != expected)
+            throw BadArgsException(op, expected, this.size)
         return this
     }
-    fun applyShortHand(xx0:Float, yy0:Float, xx1:Float, yy1:Float,
-                       path:Path, isRelative:Boolean) : BezierCurve
-    {
-        if (isEmpty) {
-            throw java.lang.RuntimeException(
-                "Attempt to \"applyShorthand\" with empty object")
-        }
-        if (isRelative != this.isRelative) {
-            throw java.lang.RuntimeException(
-                if (isRelative) "Attempt to absolute\"applyShorthand\" with relative object"
-                else "Attempt to absolute\"applyShorthand\" with relative object")
-        }
-        with (path) {
-            if (isRelative) {
-                rMoveTo(xM, yM)
-                rCubicTo(xR,yR,xx0,yy0,xx1,yy1)
+    fun apply(op : String, coords : FloatArray) {
+        when (op) {
+            "l" -> {
+                val (x,y) = coords.check(op, 2)
+                super.rLineTo(x,y)
+                previousOp = op
             }
-            else {
-                moveTo(xM,yM)
-                cubicTo(x0,y0,x1,y1,x2,y2)
+            "L" -> {
+                val (x,y) = coords.check(op, 2)
+                super.lineTo(x,y)
+                previousOp = op
             }
-            this@BezierCurve.isEmpty = true
+            "m" -> {
+                val(x,y) = coords.check(op, 2)
+                super.rMoveTo(x,y)
+                previousOp = op
+            }
+            "M" -> {
+                val(x,y) = coords.check(op, 2)
+                super.moveTo(x,y)
+                previousOp = op
+            }
+            "c" -> {
+                val (x0,y0,x1,y1,
+                    x2,y2) = coords.check(op, 6)
+                super.rCubicTo(x0,y0,x1,y1,x2,y2)
+                previousOp = op
+                xR = (2*x2) - x1
+                yR = (2*y2) - y1
+                xM = x2
+                yM = y2
+            }
+            "C" -> {
+                val (x0,y0,x1,y1,
+                    x2,y2) = coords.check(op, 6)
+                super.cubicTo(x0,y0,x1,y1,x2,y2)
+                previousOp = op
+                xR = (2*x2) - x1
+                yR = (2*y2) - y1
+                xM = x2
+                yM = y2
+            }
+            "s" -> {
+                if (previousOp != "c")
+                    throw BadArgsException(
+                        "attempt to call \"s\" after non- \"c\" operation")
+                val (x0,y0,x1,y1) = coords.check(op, 4)
+                super.rMoveTo(xM, yM)
+                super.rCubicTo(xR,yR,x0,y0,x1,y1)
+                previousOp = op
+            }
+            "S" -> {
+                if (previousOp != "C")
+                    throw BadArgsException(
+                        "attempt to call \"s\" after non- \"c\" operation")
+                val (x0,y0,x1,y1) = coords.check(op, 4)
+                super.moveTo(xM, yM)
+                super.cubicTo(xR,yR,x0,y0,x1,y1)
+                previousOp = op
+            }
+            else -> throw BadArgsException ("unrecognized operator: \"$op\"")
         }
-        return this
     }
-
-    constructor() : this(0f,0f,0f,0f,
-        0f,0f, false) {isEmpty = true}
-
 }
 
-class KvgToAndroidPaths(context: Context, val renderChar: String) {
+class KvgToAndroidPaths(context: Context, private val renderChar: String) {
     var strokePaths:Array<Path>
     val charPath = Path()
     var width = 0f
@@ -69,8 +98,8 @@ class KvgToAndroidPaths(context: Context, val renderChar: String) {
         private set
 
     // convert a string with kanjiVg paths to Android paths.
-    private fun List<String>.toPath( ): Path {
-        val path = Path()
+    private fun List<String>.strokesToPath( ): Path {
+        val svgPath = SvgPath()
         for (stroke in this) {
             val mr = "([a-zA-Z])([^a-zA-Z]+)".toRegex().find(stroke)
             val (op, floatString) = mr!!.destructured
@@ -79,50 +108,9 @@ class KvgToAndroidPaths(context: Context, val renderChar: String) {
                 .map { it.groupValues[1].toFloat() }
                 .toList()
                 .toFloatArray()
-            var previous = BezierCurve()
-            with (path) {
-                when (op) {
-                    "m" -> rMoveTo(coord[0], coord[1])
-                    "M" -> moveTo(coord[0], coord[1])
-                    "c" -> {
-                        previous = BezierCurve(
-                            coord[0], coord[1],
-                            coord[2], coord[3],
-                            coord[4], coord[5],
-                            true)
-                        .apply(this)
-                    }
-                    "C" -> {
-                        previous = BezierCurve(
-                            coord[0], coord[1],
-                            coord[2], coord[3],
-                            coord[4], coord[5],
-                            false)
-                        .apply(this)
-                    }
-                    "s" -> {
-                        previous.applyShortHand(
-                            coord[0], coord[1],
-                            coord[2], coord[3],
-                            this, true
-                        )
-                    }
-                    "S" -> {
-                        previous.applyShortHand(
-                            coord[0], coord[1],
-                            coord[2], coord[3],
-                            this, false
-                        )
-                    }
-                    "l" -> rLineTo(coord[0], coord[1])
-                    "L" -> lineTo(coord[0], coord[1])
-                    else -> throw MissingResourceException (
-                        "${renderChar}: operator:$op, Stroke:\"$stroke\"", javaClass.simpleName,
-                        "Stroke:$stroke")
-                }
-            }
+            svgPath.apply(op, coord)
         }
-        return path
+        return svgPath
     }
     init {
         val paths = mutableListOf<Path>()
@@ -152,11 +140,11 @@ class KvgToAndroidPaths(context: Context, val renderChar: String) {
                             .map { it.groupValues[0] }
                             .toList()
                         try {
-                            paths.add(strokes.toPath())
+                            paths.add(strokes.strokesToPath())
                         }
                         catch (e : Exception) {
                             throw RuntimeException(
-                                "File $fnameIn, line $lineNumber, path exception:${e.message}")
+                                "File $fnameIn, line $lineNumber, path exception:${e}")
                         }
                     }
                 }
