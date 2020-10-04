3006ff6d5c4d93706db3ce336bd021641a408a6e
sjs <sjs@sonic.net>
2020-10-04 12:29:21 -0700

    added index.html for use on kana-tutor site.

--- app/src/main/java/com/kana_tutor/kvgviewer/MainActivity.kt
+++ app/src/main/java/com/kana_tutor/kvgviewer/MainActivity.kt
@@ -22,7 +22,6 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
