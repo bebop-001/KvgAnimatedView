df396087c171797aa470f1ed47c9ffcc7913ecf5
x <x@x>
2025-05-06 00:51:24 -0700

    added seprate view so I could add a back buttonback is working..  webview

--- app/src/main/java/com/kana_tutor/utils/WebviewAlert.kt
+++ app/src/main/java/com/kana_tutor/utils/WebviewAlert.kt
@@ -7,24 +7,35 @@ import android.net.Uri
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.kana_tutor.kvgviewer.R
 
-fun Activity.webViewAlert (webView : WebView, title:String) {
-    // this as context necessary or you can get "need app compatible..."
+fun Activity.webViewAlert (webView : ConstraintLayout, title:String) {
+    // this as context necessary or you can get "need app compatibwebContainerle..."
     // app.applicationContext or app.baseContext won't work.
     val dialog = AlertDialog.Builder(this as Context)
         .setView(webView)
-        .setNegativeButton(
-            R.string.done
-        ) { dialogInterface, _ -> dialogInterface.dismiss() }
         .setCancelable(false)
+        .create()
     if (title.isNotEmpty()) dialog.setTitle(title)
     dialog.show()
+    webView.findViewById<Button>(R.id.done_btn).setOnClickListener {
+        dialog.cancel()
+    }
 }
-
 fun Activity.webviewAlert(uri:String, title:String = "") : Boolean{
-    val webView = WebView(this)
+    val webContainer = layoutInflater.inflate(
+        R.layout.web_dialog, null) as ConstraintLayout
+    val webView = webContainer.findViewById<WebView>(R.id.webview)
+    // webview doesn't know it's height so normal approach
+    // to setting height in xml won't work if it can't
+    // take full view.  Instead, get height of main
+    // display and subtract a fudge factor as
+    // a calculated height.
+    val h = resources.displayMetrics.heightPixels
+    webView.layoutParams.height = h - 350
     webView.webViewClient = object : WebViewClient() {
         @Deprecated("Deprecated in Java")
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
@@ -42,7 +53,12 @@ fun Activity.webviewAlert(uri:String, title:String = "") : Boolean{
         }
     }
     webView.loadUrl(uri)
-    webViewAlert(webView, title)
+    webContainer.findViewById<Button>(R.id.go_back_btn).setOnClickListener {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+    }
+    webViewAlert(webContainer, title)
     return true
 }
 fun Activity.webviewAlert(uri:String, resId:Int) : Boolean{
